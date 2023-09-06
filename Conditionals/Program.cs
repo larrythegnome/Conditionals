bb197226c34062c369f31cc455078f94f31e2f49
Logan <73564929+larrythegnome@users.noreply.github.com>
2023-09-06 12:13:49 -0400

    Add project files.

---
+++ Conditionals/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Conditionals
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            IfElseStuff();
+            Console.ReadLine();
+        }
+
+        static void IfElseStuff()
+        {
+            Console.WriteLine("enter gold amount: ");
+            double goldBalance = double.Parse(Console.ReadLine());
+
+            if (goldBalance >= 400)
+            {
+                Console.WriteLine("heres two ticket.");
+            }
+            else if (goldBalance >= 200)
+            {
+                Console.WriteLine("Heres one ticket");
+            }
+            else 
+            {
+                Console.WriteLine("Go away");
+            }
+        }
+    }
+}
